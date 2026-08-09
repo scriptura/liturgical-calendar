3b24e5c7610f9fb2a7fd47ac7a7c7d59644abc25
Olivier Chavarin <memoire1@gmail.com>
2026-08-09 22:37:40 +0200

    fix(fuzz): revised path

--- fuzz/target/release/build/liturgical-calendar-core-475d658b593bbb5b/build_script_build-475d658b593bbb5b.d
+++
@@ -1,5 +0,0 @@
-/home/nunn/Development/GitHub/liturgical-calendar/fuzz/target/release/build/liturgical-calendar-core-475d658b593bbb5b/build_script_build-475d658b593bbb5b.d: /home/nunn/Development/GitHub/liturgical-calendar/liturgical-calendar-core/build.rs
-
-/home/nunn/Development/GitHub/liturgical-calendar/fuzz/target/release/build/liturgical-calendar-core-475d658b593bbb5b/build_script_build-475d658b593bbb5b: /home/nunn/Development/GitHub/liturgical-calendar/liturgical-calendar-core/build.rs
-
-/home/nunn/Development/GitHub/liturgical-calendar/liturgical-calendar-core/build.rs:
--- fuzz/target/x86_64-unknown-linux-gnu/release/deps/validate_header-9cd1d711023df2b4.d
+++
@@ -1,5 +0,0 @@
-/home/nunn/Development/GitHub/liturgical-calendar/fuzz/target/x86_64-unknown-linux-gnu/release/deps/validate_header-9cd1d711023df2b4.d: fuzz_targets/validate_header.rs
-
-/home/nunn/Development/GitHub/liturgical-calendar/fuzz/target/x86_64-unknown-linux-gnu/release/deps/validate_header-9cd1d711023df2b4: fuzz_targets/validate_header.rs
-
-fuzz_targets/validate_header.rs:
